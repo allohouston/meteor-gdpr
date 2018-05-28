22415d64691a724f2f573da64c0a88249281c831
Victor Parpoil <vparpoil@gmail.com>
2018-05-28 11:09:27 +0200

    adding git repo reference in package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
     // Brief, one-line summary of the package.
     summary: '',
     // URL to the Git repository containing the source code for this package.
-    git: '',
+    git: 'https://github.com/yellowsquaresas/meteor-gdpr',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
@@ -43,7 +43,6 @@ Package.onUse(function (api) {
     api.addFiles('lib/GDPR.js', 'client');
     api.export('GDPRconfig', 'client');
 
-    //api.mainModule('gdpr.js', 'client');
 });
 
 Package.onTest(function (api) {
